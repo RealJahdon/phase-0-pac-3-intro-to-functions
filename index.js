fd43954fdfc97b84f6857ac9418465b927d4af9a
Jahdon Ervin <Jahdonervin@gmail.com>
2022-11-15 17:45:16 -0500

    commit tree f233ea0ce4cf2f718affd4ad0fdbd1b0fcc70826

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Follow along with the examples here
+function sayHelloclass() {
+    console.log("Hello, class!");
+  }
+  sayHelloclass();
+  function doSomething(Wave) {
+    console.log(Wave);
+  }
+  
+  doSomething("Wave");
+  function sayHelloTo(flatironschool) {
+    console.log(`Hello, ${flatironschool}!`);
+  }
+  
+  sayHelloTo("Flatiron school"); // "Hello, Flatiron school!"
+  function say(greeting, firstName) {
+    console.log(`${greeting}, ${firstName}!`);
+  }
+  say("Goodbye", "class");
+  function doSomething(Wave) {
+    console.log(Wave);
+  }
+  
+  doSomething("Wave");
+  function say(greeting, firstName) {
+    console.log(`${greeting}, ${firstName}!`);
+  }
+  say("Goodbye", "Flatiron School!");
